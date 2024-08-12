42e4fe2f12ff52e89c3063259b5327df8caa8b80
HaiAnh1905 <haianh198005@gmail.com>
2024-08-12 17:28:59 +0700

    thrd-commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let arr = [1,2,5,6,7,10,15]
 
+//==============================================
+
 // map()
 // là phương thức được dùng để thao tác với các phần tử trên 1 mảng và trả về 1 giá trị thay đổi dựa trên mảng đó.
 // demo: 
@@ -13,6 +15,8 @@ console.log(arr1); // giá trị trả về [2,4,10,12,14,20,30]
 const arr2 = arr.filter(items => items % 2 == 0);
 console.log(arr2); // giá trị trả về [2, 6, 10 ]
 // giải thích:  hàm filter sẽ lấy ra các phần tử chia hết cho 2 có trong arr và truyền vào mảng arr2
+
+//==============================================
 
 // reduce()
 // là 1 phương thức đặc biệt được sử dụng để thực thi 1 hàm lên các phần tử của 1 mảng(từ trái qua phải) với 1 biến tích lũy để thu về 1 giá trị duy nhất
@@ -36,6 +40,8 @@ console.log(result); // giá trị trả về 46
 // chiều từ trái qua phải.
 // khi không còn giá trị nào thì phương thức reduce() sẽ trả về tổng giá trị.
 
+//==============================================
+
 // slice()
 // tác dụng của hàm này là cắt bỏ 1 phần của array và cần lưu ý là hàm slice() sẽ tạo ra 1 mảng mới thay vì thực hiện thay đổi trực tiến trên mảng được cho
 // cấu trúc: array.slice(from, until)
@@ -45,6 +51,8 @@ console.log(result); // giá trị trả về 46
 const arr3 = arr.slice(3, 6)
 console.log(arr3); // giá trị trả về [ 6, 7, 10 ]
 // giải thích: mảng arr3 sẽ được trả về từ giá trị thứ 3 của mảng đến giá trị thứ 5 của mảng arr
+
+//==============================================
 
 //splice:
 // tác dụng tương tử hàm slice(), tuy nhiên ngược lại vs slice(), splice sẽ thay đổi trực tiếp trên đối tượng được cho sẵn.
@@ -58,6 +66,8 @@ console.log(arr); // giá trị trả về [ 1, 7, 10, 15 ]
 // arr = [1,2,5,6,7,10,15]
 // giải thích: vì đối số index được truyền vào là 1, và noe được truyền vào là 3 nên hàm splice sẽ loại bỏ các phần tử 2, 5, 6
 arr = [1,2,5,6,7,10,15]
+
+//==============================================
 
 //find():
 // hàm find() sẽ lần lượt sử dụng các phần tử của mảng để thực hiện hàm kiểm tra cho đến khi có giá trị thõa mãn và hàm kiểm tra trả
@@ -82,6 +92,8 @@ console.log(index1); // giá trị trả về 4
 //  arr = [1,2,5,6,7,10,15]
 // giải thích: bởi vì tại giá trị bằng 7 thõa mãn điều kiện > 6 nên hàm findIndex() sẽ trả về vị trí của nó tại 4
 
+//==============================================
+
 //forEach()
 // là 1 hàm phương thức có sẵn trong mảng, đúng như tên gọi của nó, ta sẽ lặp qua từng phần tử có trong mảng và xử lý chúng theo thứ tự
 // cú pháp: forEach(item, index, array)
@@ -99,6 +111,8 @@ fruit.forEach((item) => {
 // I love pineapple
 // I love orange
 // I love mango
+
+//==============================================
 
 // some()
 // hàm some() có nhiệm vụ lặp qua tất cả các phần tử của mảng, mỗi lần lặp nó sẽ truyền giá trị của phần tử đang lặp vào hàm callback
@@ -120,10 +134,43 @@ myFunc()// kết quả trả về: Rớt
 // giải thích: bởi vì trong mảng score tồn tại 1 giá trị nhỏ hơn 5 -> hàm callback() thõa mãn điều kiện và trả về true -> hàm some() trả về là True
 // mà nếu kết quả trả về là true thì sẽ trả về là ko đỗ hay là rớt
 
+//==============================================
+
 //sort()
 // hàm sort là một method thuộc đối tượng array trong js, được đùng dể sắp xếp các phần tử trong mảng tăng dần
 // hoặc giảm dần theo số thứ tự trong bảng mã ascii, hoặc theo quy tắc trong callback function.
 // lưu ý: hàm sort sẽ gặp rắc rối trong việc sắp xếp các con số sẽ ko được chính xác( ví dụ 20 vs 100 thì 20 > 100 vì 2 > 1)
 // nên cần thêm một hàm so sánh riêng biệt để tránh trường hợp trên, trong khi đó thì sắp xếp các chữ cái rất chính xác
 // demo:
+// Sap xep chu cai
+// const fruit = ['banana','apple','pineapple','orange','mango']
+fruit.sort();
+console.log(fruit); // trả về: [ 'apple', 'banana', 'mango', 'orange', 'pineapple' ]
+// Sắp xếp các con số
+const num = [2,30, 100, 9]
+num.sort();
+console.log(num);// trả về: [ 100, 2, 30, 9 ]
+// giải thích: kết quả trả về là sai bởi vì hàm sort nghĩ là 100 < 2 như 1<2 nên mới sắp xếp 100 lên trước 2, 
+// để tránh tính trạng như trên thì ta làm như sau
+num.sort((firstEl, secondEl) => {
+    let huh;
+    // console.log(secondEl, firstEl);
     
+    secondEl > firstEl ? huh = -1 : huh = 0;
+    return huh;
+})
+console.log(num);// trả về: [2,9,30,100]
+// giải thích: đoạn này em ko hiểu lắm @@
+
+//==============================================
+
+// every()
+// hàm every() là 1 phương thức dành cho đối tượng cho js, công dụng của hàm này là để ktra tính toàn thể của 1 mảng, nếu có 1 phần tử trong mảng ko
+// thõa mãn điều kiện thì sẽ trả về false, nếu thõa toàn bộ thì trả về true
+// cú pháp: every(function)
+// demo:
+const evenNum = [2,4,6,10,13,14,18];
+console.log(evenNum.every((item) => {
+    return item % 2 == 0
+}));// kết quả trả về: false
+//giải thích: dựa vào định nghĩa của every, bởi vì có 1 giá trị là 13 ko thõa mãn điều kiện nên hàm every() trả về false
